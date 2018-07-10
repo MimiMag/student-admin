f9315c7a70267e5918d6bea8c1b64ddcb2e5e719
MimiMagusin <mimi_magusin@hotmail.com>
2018-07-10 16:49:46 +0200

    Set up project

--- students-service/src/index.ts
+++ students-service/src/index.ts
@@ -1,14 +1,12 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import { Student } from "./entity/Student";
+// import { Student } from "./entity/Student";
 import { createKoaServer } from "routing-controllers"
-import { StudentController } from "./controllers/StudentsController"
-import { seeding } from "./migration/seeds"
+import { StudentController } from "./students/StudentsController"
+// import { seeding } from "./seeds"
 
 createConnection().then(async connection => {
-  seeding()
-  const students = await connection.manager.find(Student);
-  console.log("Loaded students: ", students);
+  // seeding()
   const port = process.env.PORT || 4001
 
   const app = createKoaServer({

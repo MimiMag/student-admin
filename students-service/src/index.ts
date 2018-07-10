c33af147edb7d0699cef60a2a1cee9f699ea13e3
MimiMagusin <mimi_magusin@hotmail.com>
2018-07-10 17:03:21 +0200

    remove comment

--- students-service/src/index.ts
+++ students-service/src/index.ts
@@ -1,12 +1,9 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-// import { Student } from "./entity/Student";
 import { createKoaServer } from "routing-controllers"
 import { StudentController } from "./students/StudentsController"
-// import { seeding } from "./seeds"
 
 createConnection().then(async connection => {
-  // seeding()
   const port = process.env.PORT || 4001
 
   const app = createKoaServer({

c01989b18fb3a52f39bc93cf0e3e7a27db01bd9d
MimiMagusin <mimi_magusin@hotmail.com>
2018-07-13 20:39:33 +0200

    Some more navbar setup

--- student-admin/src/nav-components/Navbar.jsx
+++ student-admin/src/nav-components/Navbar.jsx
@@ -4,6 +4,9 @@ import Dropdown from './Dropdown';
 
 export const NavBar = () => {
   return <div className='navbar'>
+    <h2 className="subtitle">Display Results</h2>
+    <Dropdown />
+    <h2 className="subtitle">Filter students</h2>
     <Dropdown />
   </div>
 }
